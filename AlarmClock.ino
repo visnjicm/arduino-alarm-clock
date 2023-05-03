7adb917c50390599b56be5c0bc58f135697d0463
Marko Visnjic <markovisnjic12@gmail.com>
2023-05-02 21:13:21 -0400

    Added seconds, minutes, and hours incrementation

--- AlarmClock.ino
+++ AlarmClock.ino
@@ -1,21 +1,31 @@
-// Time - Version: Latest 
 #include <TimeLib.h>
-
-// include the library code:
 #include <LiquidCrystal.h>
 
 #define ARR_SIZE 15
 #define LOOP_SIZE 20
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
+
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
 char startDate[ARR_SIZE] = __DATE__;
 char startTime[ARR_SIZE] = __TIME__;
 char myDate[3][ARR_SIZE];
 char myTime[3][ARR_SIZE];
 char prev_char;
+
+String hoursString = "";
+String minutesString = "";
+String secondsString = "";
+
+int hours = hoursString.toInt();
+int minutes = minutesString.toInt();
+int seconds = secondsString.toInt();
+int addSeconds;
+int prev_addSeconds;
+
+
 
 void setup() {
   // set up the LCD's number of columns and rows:
@@ -24,11 +34,44 @@ void setup() {
 
   parse_startDate();
   parse_startTime();
+  hoursString.concat(myTime[0][0]);
+  hoursString.concat(myTime[0][1]);
+  minutesString.concat(myTime[1][0]);
+  minutesString.concat(myTime[1][1]);
+  secondsString.concat(myTime[2][0]);
+  secondsString.concat(myTime[2][1]);
+  hours = hoursString.toInt();
+  minutes = minutesString.toInt();
+  seconds = secondsString.toInt();
+  Serial.println("Done");
 }
 
+
+
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
+
+  addSeconds = millis()/1000;
+  if (addSeconds != prev_addSeconds){
+    seconds ++;
+  }
+  prev_addSeconds = addSeconds;
+
+
+  if (seconds == 60){
+    seconds = 0;
+    minutes ++;
+  }
+
+  if (minutes == 60){
+    minutes = 0;
+    hours++;
+  }
+
+  if (hours == 24){
+    hours = 0;
+  }
+
+
   lcd.setCursor(0, 0);
   lcd.print(myDate[0]);
   lcd.print(" ");
@@ -39,12 +82,18 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print(myTime[0]);
   lcd.print("h");
-  lcd.print(myTime[1]);
+  lcd.print(String(minutes));
   lcd.print("m");
-  lcd.print(myTime[2]);
-  lcd.print("s");
+  lcd.print(String(seconds));
+  lcd.print("s ");
+
 }
 
+
+
+
+
+//User Functions
 void parse_startTime()
 {
   int i = 0;
